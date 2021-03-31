ebfb8c6317bc65d8d19a87e6ae24c37afdae78e6
erlenkh <erlenkh@stud.ntnu.no>
2021-03-31 23:17:02 +0200

    cleanup

--- controller.m
+++ controller.m
@@ -35,7 +35,6 @@ function controls = controller(z,z_ref,theta_error_prev)
     K_l = 1 * scaling;
     K_sd = 4 * scaling;
 
-   
     V = K_v * x_error;
           
     V = min(max(V,0.5),30);    
@@ -49,33 +48,5 @@ function controls = controller(z,z_ref,theta_error_prev)
     controls = [V, omega_st,transpose(b_e), omega_st_no_shrink, theta_error_der];
     
 end
-    
-%     K_v = 10;   
-%     K_s = 20;
-%     K_l = 0;
-    
-%      K_v = 1;   %0.5
-%     K_s = 150;
-%     K_l = 50;
 
-   % HOLY TUNING: 
-    
-%     scaling = 0.2;
-%     K_v = 7 *scaling;   
-%     K_s = 13 *scaling; %13
-%     K_l = 0 * scaling;
-%     
-%     K_sd = 4 * scaling;
-
-    %PREVIOUS Z;
-%     x_prev = z_prev(1);  y_prev = z_prev(2); theta_prev = z_prev(3);
-%     x_ref_prev = z_ref_prev(1); y_ref_prev = z_ref_prev(2); theta_ref_prev = z_ref_prev(3);
-%     
-%     W_e_prev = [x_ref_prev - x_prev; y_ref_prev - y_prev; theta_ref_prev - theta_prev];
-%     
-%     rot_mat_prev = [cos(theta_prev) sin(theta_prev) 0;
-%                -sin(theta_prev) cos(theta_prev) 0;
-%                0               0      1;];
-   
-% V = min(max(V,0.5),10);
 
